002924f2000db457641e9243bf6ed6cb7d9108a0
CUSTcoding <custodiotitossetitosse@gmail.com>
2025-11-06 15:51:30 -0600

    feat: start the project whit spring boot

---
+++ src/main/java/cust/coding/m_pesa_clone/MPesaCloneApplication.java
@@ -0,0 +1,13 @@
+package cust.coding.m_pesa_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MPesaCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MPesaCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cust/coding/m_pesa_clone/MPesaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package cust.coding.m_pesa_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPesaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
